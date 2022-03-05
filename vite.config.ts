efc9abe0103463b5a4ec61d264732a6893237132
william vegas <elgeokareem@pop-os.localdomain>
2022-03-05 16:44:06 -0400

    PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { VitePWA } from 'vite-plugin-pwa'
 import { fileURLToPath, URL } from "url";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({})
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
