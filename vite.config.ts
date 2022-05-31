d54c9e311fad9fe87fd6cd2b1eeb1334e138436e
william vegas <elkareem123@gmail.com>
2022-05-31 18:15:44 -0400

    updated packages, added tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,14 @@
 import { defineConfig } from "vite";
-import { VitePWA } from 'vite-plugin-pwa'
+import { VitePWA } from "vite-plugin-pwa";
 import { fileURLToPath, URL } from "url";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    VitePWA({})
-  ],
+  plugins: [vue(), VitePWA({})],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
+      "@": fileURLToPath(new URL("./src", import.meta.url))
+    }
+  }
 });
